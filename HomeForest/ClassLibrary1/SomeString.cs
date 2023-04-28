e1fc95aaf2d4ed43f69c2b582c5808f46ff351e4
barsik-v-nosochkah <126867154+barsik-v-nosochkah@users.noreply.github.com>
2023-04-28 14:42:28 +0300

    Add maui runner and test tests

---
+++ HomeForest/ClassLibrary1/SomeString.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class SomeString
+    {
+        public string ConcatenatedString(string one, string two)
+        {
+            return one + two;
+        }
+
+        public string ConcatenatedStringWithSeparator(string one, string two)
+        {
+            return one + "/" + two;
+        }
+    }
+}
---
+++ HomeForest/HomeForest.IntegrationTests/TestClass.cs
@@ -0,0 +1,37 @@
+﻿using ClassLibrary1;
+using Xunit;
+
+namespace HomeForest.IntegrationTests
+{
+    public class TestClass
+    {
+        private readonly SomeString _someString;
+
+        public TestClass()
+        {
+            _someString = new SomeString();
+        }
+
+        [Fact]
+        public void ConcatTwoStrings()
+        {
+            var one = "one";
+            var two = "two";
+
+            var res = _someString.ConcatenatedString(one, two);
+
+            Assert.Equal("onetwo", res);
+        }
+
+        [Fact]
+        public void ConcatTwoStrings_OneEmpty()
+        {
+            var one = "one";
+            var two = string.Empty;
+
+            var res = _someString.ConcatenatedString(one, two);
+
+            Assert.Equal("one", res);
+        }
+    }
+}
